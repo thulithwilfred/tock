33e5c9f541be7515bd66c0268a2212518667cfd6
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-01-13 16:29:57 +1000

    lowrisc/spi_host: updated host registers (OT)
    Updated host SpiHostRegisters to spec opentitan SpiHost.
    Testing: passed `make ci-job-chips`
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- chips/lowrisc/src/spi_host.rs
+++ chips/lowrisc/src/spi_host.rs
@@ -1,6 +1,6 @@
 //! Serial Peripheral Interface (SPI) Driver
 use core::option::Option;
-use kernel::debug;
+//use kernel::debug;
 use kernel::hil;
 use kernel::hil::spi::{ClockPhase, ClockPolarity};
 use kernel::utilities::cells::OptionalCell;
@@ -10,97 +10,133 @@ use kernel::utilities::registers::{
 use kernel::utilities::StaticRef;
 use kernel::ErrorCode;
 
-//TODO SPI: Update reg
-//TODO Add resreves for gaps
 //TODO TEST: make ci-job-chips
 register_structs! {
     pub SpiHostRegisters {
-        (0x000 => intr_state: ReadWrite<u32, INTR::Register>),
-        (0x004 => intr_enable: ReadWrite<u32, INTR::Register>),
-        (0x008 => intr_test: WriteOnly<u32, INTR::Register>),
-        (0x00C => control: ReadWrite<u32, CONTROL::Register>),
-        (0x010 => cfg: ReadWrite<u32, CFG::Register>),
-        (0x014 => fifo_level: ReadWrite<u32, FIFO_LEVEL::Register>),
-        (0x018 => async_fifo_level: ReadOnly<u32, ASYNC_FIFO_LEVEL::Register>),
-        (0x01C => status: ReadOnly<u32, STATUS::Register>),
-        (0x020 => rxf_ptr: ReadWrite<u32, RXF_PTR::Register>),
-        (0x024 => txf_ptr: ReadWrite<u32, TXF_PTR::Register>),
-        (0x028 => rxf_addr: ReadWrite<u32, RXF_ADDR::Register>),
-        (0x02C => txf_addr: ReadWrite<u32, TXF_ADDR::Register>),
-        (0x030 => _reserved0),
-        (0x800 => buffer: [ReadWrite<u32>; 512]),
-        (0x1000 => @END),
+        //SPI: Interrupt State Register
+        (0x000 => intr_state: ReadWrite<u32, intr::Register>),
+        //SPI: Interrupt Enable Register
+        (0x004 => intr_enable: ReadWrite<u32, intr::Register>),
+        //SPI: Interrupt Test Register
+        (0x008 => intr_test: WriteOnly<u32, intr::Register>),
+        //SPI: Alert Test Register
+        (0x00c => alert_test: WriteOnly<u32, alert_test::Register>),
+        //SPI: Control register
+        (0x010 => control: ReadWrite<u32, ctrl::Register>),
+        //SPI: Status register
+        (0x014 => status: ReadOnly<u32, status::Register>),
+        //SPI: Configuration options register.
+        (0x018 => config_opts: ReadWrite<u32, conf_opts::Register>),
+        //SPI: Chip-Select ID
+        (0x01c => csid: ReadWrite<u32, csid_ctrl::Register>),
+        //SPI: Command Register
+        (0x020 => command: WriteOnly<u32, command::Register>),
+        //SPI: Received Data
+        (0x024 => rx_data: ReadWrite<u32, rx_data::Register>),
+        //SPI: Transmit Data
+        (0x028 => tx_data: WriteOnly<u32, tx_data::Register>),
+        //SPI: Controls which classes of errors raise an interrupt.
+        (0x02c => err_en: ReadWrite<u32, err_en::Register>),
+        //SPI: Indicates that any errors that have occurred
+        (0x030 => err_status: ReadWrite<u32, err_status::Register>),
+        //SPI: Controls which classes of SPI events raise an interrupt
+        (0x034 => event_en: ReadWrite<u32, event_en::Register>),
+        (0x38 => @END),
     }
 }
 
-//TODO SPI: Update these to match
 register_bitfields![u32,
-    INTR [
-        RXF OFFSET(0) NUMBITS(1) [],
-        RXLVL OFFSET(1) NUMBITS(1) [],
-        TXLVL OFFSET(2) NUMBITS(1) [],
-        RXERR OFFSET(3) NUMBITS(1) [],
-        RXOVERFLOW OFFSET(4) NUMBITS(1) [],
-        TXUNDERFLOW OFFSET(5) NUMBITS(1) []
+    intr [
+        ERROR OFFSET(0) NUMBITS(1) [],
+        SPI_EVENT OFFSET(1) NUMBITS(1) [],
     ],
-    CONTROL [
-        ABORT OFFSET(0) NUMBITS(1) [],
-        MODE OFFSET(4) NUMBITS(2) [],
-        RST_TXFIFO OFFSET(16) NUMBITS(1) [],
-        RST_RXFIFO OFFSET(17) NUMBITS(2) []
+    alert_test [
+        FETAL_FAULT OFFSET(0) NUMBITS(1) [],
     ],
-    CFG [
-        CPOL OFFSET(0) NUMBITS(1) [],
-        CPHA OFFSET(1) NUMBITS(1) [],
-        TX_ORDER OFFSET(2) NUMBITS(1) [],
-        RX_ORDER OFFSET(3) NUMBITS(1) [],
-        TIMER_V OFFSET(8) NUMBITS(8) []
+    ctrl [
+        RX_WATERMARK OFFSET(0) NUMBITS(8) [],
+        TX_WATERMARK OFFSET(15) NUMBITS(8) [],
+        //28:16 RESERVED
+        OUTPUT_EN OFFSET(29) NUMBITS(1) [],
+        SW_RST OFFSET(30) NUMBITS(1) [],
+        SPIEN OFFSET(31) NUMBITS(1) []
     ],
-    FIFO_LEVEL [
-        RXLVL OFFSET(0) NUMBITS(16) [],
-        TXLVL OFFSET(16) NUMBITS(16) []
+    status [
+        TXQD OFFSET(0) NUMBITS(8) [],
+        RXQD OFFSET(15) NUMBITS(8) [],
+        CMDQD OFFSET(16) NUMBITS(1) [],
+        RXWM OFFSET(20) NUMBITS(1) [],
+        BYTEORDER OFFSET(22) NUMBITS(1) [],
+        RXSTALL OFFSET(23) NUMBITS(1) [],
+        RXEMPTY OFFSET(24) NUMBITS(1) [],
+        RXFULL OFFSET(25) NUMBITS(1) [],
+        TXWM OFFSET(26) NUMBITS(1) [],
+        TXSTALL OFFSET(27) NUMBITS(1) [],
+        TXEMPTY OFFSET(28) NUMBITS(1) [],
+        TXFULL OFFSET(29) NUMBITS(1) [],
+        ACTIVE OFFSET(30) NUMBITS(1) [],
+        READY OFFSET(31) NUMBITS(1) [],
     ],
-    ASYNC_FIFO_LEVEL [
-        RXLVL OFFSET(0) NUMBITS(8) [],
-        TXLVL OFFSET(16) NUMBITS(8) []
+    conf_opts [
+        CLKDIV_0 OFFSET(0) NUMBITS(16) [],
+        CSNIDLE_0 OFFSET(16) NUMBITS(3) [],
+        CSNTRAIL_0 OFFSET(20) NUMBITS(3) [],
+        CSNLEAD_0 OFFSET(24) NUMBITS(3) [],
+        //28 Reserved
+        FULLCYC_0 OFFSET(29) NUMBITS(1) [],
+        CPHA_0 OFFSET(30) NUMBITS(1) [],
+        CPOL_0 OFFSET(31) NUMBITS(1) [],
     ],
-    STATUS [
-        RXF_FULL OFFSET(0) NUMBITS(1) [],
-        RXF_EMPTY OFFSET(1) NUMBITS(1) [],
-        TXF_FULL OFFSET(2) NUMBITS(1) [],
-        TXF_EMPTY OFFSET(3) NUMBITS(1) [],
-        ABORT_DONE OFFSET(4) NUMBITS(1) [],
-        CSB OFFSET(5) NUMBITS(1) []
+    csid_ctrl [
+        CSID OFFSET(0) NUMBITS(32) [],
     ],
-    RXF_PTR [
-        RPTR OFFSET(0) NUMBITS(16) [],
-        WPTR OFFSET(16) NUMBITS(16) []
+    command [
+        LEN OFFSET(0) NUMBITS(8) [],
+        CSAAT OFFSET(9) NUMBITS(1) [],
+        SPEED OFFSET(10) NUMBITS(2) [],
+        DIRECTION OFFSET(12) NUMBITS(2) [],
     ],
-    TXF_PTR [
-        RPTR OFFSET(0) NUMBITS(16) [],
-        WPTR OFFSET(16) NUMBITS(16) []
+    rx_data [
+        DATA OFFSET(0) NUMBITS(32) [],
     ],
-    RXF_ADDR [
-        BASE OFFSET(0) NUMBITS(16) [],
-        LIMIT OFFSET(16) NUMBITS(16) []
+    tx_data [
+        DATA OFFSET(0) NUMBITS(32) [],
     ],
-    TXF_ADDR [
-        BASE OFFSET(0) NUMBITS(16) [],
-        LIMIT OFFSET(16) NUMBITS(16) []
-    ]
+    err_en [
+        CMDBUSY OFFSET(0) NUMBITS(1) [],
+        OVERFLOW OFFSET(1) NUMBITS(1) [],
+        UNDERFLOW OFFSET(2) NUMBITS(1) [],
+        CMDINVAL OFFSET(3) NUMBITS(1) [],
+        CSIDINVAL OFFSET(4) NUMBITS(1) [],
+    ],
+    err_status [
+        CMDBUSY OFFSET(0) NUMBITS(1) [],
+        OVERFLOW OFFSET(1) NUMBITS(1) [],
+        UNDERFLOW OFFSET(2) NUMBITS(1) [],
+        CMDINVAL OFFSET(3) NUMBITS(1) [],
+        CSIDINVAL OFFSET(4) NUMBITS(1) [],
+        ACCESSINVAL OFFSET(5) NUMBITS(1) [],
+    ],
+    event_en [
+        RXFULL OFFSET(0) NUMBITS(1) [],
+        TXEMPTY OFFSET(1) NUMBITS(1) [],
+        RXWM OFFSET(2) NUMBITS(1) [],
+        TXWM OFFSET(3) NUMBITS(1) [],
+        READY OFFSET(4) NUMBITS(1) [],
+        IDLE OFFSET(5) NUMBITS(1) [],
+    ],
 ];
 
 pub struct SpiHost {
     _registers: StaticRef<SpiHostRegisters>,
-
-    client: OptionalCell<&'static dyn hil::spi::SpiSlaveClient>,
+    _client: OptionalCell<&'static dyn hil::spi::SpiSlaveClient>,
 }
 
 impl SpiHost {
     pub const fn new(base: StaticRef<SpiHostRegisters>) -> Self {
         SpiHost {
             _registers: base,
-            client: OptionalCell::empty(),
+            _client: OptionalCell::empty(),
         }
     }
     //TODO CB: handle spi interrupts
