7c4dea35661e7095eb559d8cb35be2e7747c3beb
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-01-14 12:03:31 +1000

    lowrisc/earlgrey: spi.rs removing older files
    These files were renamed to spi_host.rs
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- chips/earlgrey/src/spi.rs
+++
@@ -1,5 +0,0 @@
-use kernel::utilities::StaticRef;
-use lowrisc::spi::SpiDeviceRegisters;
-
-pub const SPIHOST0_BASE: StaticRef<SpiDeviceRegisters> =
-    unsafe { StaticRef::new(0x4006_0000 as *const SpiDeviceRegisters) };
