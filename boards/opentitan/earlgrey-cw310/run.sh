930f75302a287a29bb6e981dba79ebbc824c55a1
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-07-18 14:41:19 +1000

    Makefile: point qemu paths to devel
    Points to an external development qemu repo (over nested qemu in tock
    tools)
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- boards/opentitan/earlgrey-cw310/run.sh
+++ boards/opentitan/earlgrey-cw310/run.sh
@@ -28,5 +28,5 @@ elif [[ "${OPENTITAN_TREE}" != "" ]]; then
 	riscv64-linux-gnu-objcopy --output-target=binary bundle.elf binary
 	${OPENTITAN_TREE}/util/fpga/cw310_loader.py --firmware binary
 else
-	../../../tools/qemu/build/qemu-system-riscv32 -M opentitan -bios ../../../tools/qemu-runner/opentitan-boot-rom.elf -nographic -serial stdio -monitor none -semihosting -kernel "${1}"
+	/home/twilfred/wdc/qemu/build/qemu-system-riscv32 -M opentitan -bios ../../../tools/qemu-runner/opentitan-boot-rom.elf -nographic -serial stdio -monitor none -semihosting -kernel "${1}"
 fi
