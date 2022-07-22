2c1d1fe3495040749149e10f3082719ebe0aac5a
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-07-22 14:16:49 +1000

    opentitan: makefile/run.sh: fixup paths to OT targets
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- boards/opentitan/earlgrey-cw310/run.sh
+++ boards/opentitan/earlgrey-cw310/run.sh
@@ -3,14 +3,14 @@
 BUILD_DIR="verilator_build/"
 
 if [[ "${VERILATOR}" == "yes" ]]; then
-		if [ -d "$BUILD_DIR" ]; then
-			# Cleanup before we build again
-			printf "\n[CW-130: Verilator Tests]: Cleaning up verilator_build...\n\n"
-			rm -R "$BUILD_DIR"/*
-		else
-			printf "\n[CW-130: Verilator Tests]: Setting up verilator_build...\n\n"
-			mkdir "$BUILD_DIR"
-		fi
+	if [ -d "$BUILD_DIR" ]; then
+		# Cleanup before we build again
+		printf "\n[CW-130: Verilator Tests]: Cleaning up verilator_build...\n\n"
+		rm -R "$BUILD_DIR"/*
+	else
+		printf "\n[CW-130: Verilator Tests]: Setting up verilator_build...\n\n"
+		mkdir "$BUILD_DIR"
+	fi
 	# Copy in and covert from cargo test output to binary
 	${OBJCOPY} ${1} "$BUILD_DIR"/earlgrey-cw310-tests.elf
 	${OBJCOPY} --output-target=binary "$BUILD_DIR"/earlgrey-cw310-tests.elf "$BUILD_DIR"/earlgrey-cw310-tests.bin
@@ -22,7 +22,7 @@ if [[ "${VERILATOR}" == "yes" ]]; then
 	${OPENTITAN_TREE}/bazel-out/k8-fastbuild/bin/hw/build.verilator_real/sim-verilator/Vchip_sim_tb \
 		--meminit=rom,${OPENTITAN_TREE}/bazel-out/k8-fastbuild-ST-97f470ee3b14/bin/sw/device/lib/testing/test_rom/test_rom_sim_verilator.scr.39.vmem \
 		--meminit=flash,./"$BUILD_DIR"/binary.64.vmem \
-		--meminit=otp,${OPENTITAN_TREE}/bazel-out/k8-fastbuild/bin/sw/device/tests/otp_ctrl_smoketest_sim_verilator.runfiles/lowrisc_opentitan/hw/ip/otp_ctrl/data/img_rma.vmem
+		--meminit=otp,${OPENTITAN_TREE}/bazel-out/k8-fastbuild/bin/hw/ip/otp_ctrl/data/rma_image_verilator.vmem
 elif [[ "${OPENTITAN_TREE}" != "" ]]; then
 	riscv64-linux-gnu-objcopy --update-section .apps=${APP} ${1} bundle.elf
 	riscv64-linux-gnu-objcopy --output-target=binary bundle.elf binary
