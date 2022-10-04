18588d4f5ef4adb8798a05a4400f807014e939db
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-10-04 16:58:34 +1000

    REBASE TO DRIVER
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- chips/lowrisc/src/pwm.rs
+++ chips/lowrisc/src/pwm.rs
@@ -1,11 +1,16 @@
 //! PWM Driver
+//! TODO: Need to implement PinMux to test this on Verilator
+//!       which only exposes GPIO0.
+//! Come back to this when PinMux is implemented
 
 use kernel::platform;
 use kernel::utilities::registers::interfaces::{Readable, Writeable};
 use kernel::utilities::registers::{register_bitfields, register_structs, ReadWrite, WriteOnly};
+use kernel::utilities::registers::interfaces::ReadWriteable;
 use kernel::utilities::StaticRef;
 use kernel::ErrorCode;
 use kernel::hil;
+use core::cell::Cell;
 
 // Refer: https://docs.opentitan.org/hw/ip/pwm/doc/#register-table
 register_structs! {
@@ -23,7 +28,7 @@ register_structs! {
         //PWM: Basic PWM Channel Parameters
         (0x014 => pwm_param: [ReadWrite<u32, PWM_PARAM::Register>; 6]),
         // PWM: Controls the duty_cycle of each channel.
-        (0x02c => duty_cyle: [ReadWrite<u32, DUTY_CYCLE::Register>; 6]),
+        (0x02c => duty_cycle: [ReadWrite<u32, DUTY_CYCLE::Register>; 6]),
         // PWM: Controls the duty_cycle of each channel.
         (0x044 => blink_param: [ReadWrite<u32, BLINK_PARAM::Register>; 6]),
         (0x05C => @END),
@@ -87,6 +92,106 @@ impl PWMCtrl {
             registers: base,
         }
     }
+    /// # Arguments
+    /// in_clk: peripheral clock
+    /// fpwm: pwm suggested clock
+    /// dc_resn_bits: num bits that define dc_resn (dc_resn + 1)
+    fn calc_clk_div(&self, in_clk: u32, fpwm: u32, dc_resn_bits: u32) -> Result<u32, ErrorCode> {
+        if dc_resn_bits > 16 || fpwm >= in_clk{
+            return Err(ErrorCode::INVAL);
+        }
+        // Only allowed 27 bits for clk_div
+        let clk_div_max: u32 = 134217727; // 2^27 - 1
+        let clk_div: u32 = (in_clk / ((2 << dc_resn_bits) * (fpwm))) - 1;
+        
+        if clk_div > clk_div_max {
+            return Err(ErrorCode::INVAL);
+        }
+
+        Ok(clk_div)
+    }
+
+    fn pwm_setup(&self, fpwm: u32, duty_cycle: usize) -> Result<(), ErrorCode> {
+        let regs = self.registers;
+        
+        if regs.regwen.is_set(REGWEN::REGWEN) {
+            // CFG Locked
+            return Err(ErrorCode::NOSUPPORT);
+        }
+
+        if fpwm == 0 {
+            for i in 0..6 {
+                // Stop all 6 channels
+                self.pwm_chan_stop(i);
+            }
+            return Ok(());
+        }
+
+        let in_clk = 2_500_000;
+
+        // Disables and resets the phase counter
+        regs.cfg.modify(CFG::CNTR_EN::CLEAR);
+
+        // Solve for CLK_DIV to get desired frequency with DC_RESN=8 (default)
+        if let Ok(clk_div) = self.calc_clk_div(in_clk as u32, fpwm, 7 + 1) {
+            // Found matching config
+            regs.cfg.modify(CFG::CLK_DIV.val(clk_div as u32) + CFG::DC_RESN.val(7));
+        } else {
+            // Cannot achieve desired config
+            return Err(ErrorCode::INVAL);
+        }
+        
+        // Enable the PWM phase counter
+        regs.cfg.modify(CFG::CNTR_EN::SET);
+
+        Ok(())
+    }
+
+    fn pwm_chan_start(&self, channel: usize, duty_cycle: u32) -> Result<(), ErrorCode> {
+        if channel >= PWM_MAX_CHANS {
+            // Channel index start from 0 
+            return Err(ErrorCode::INVAL);
+        }
+
+        let regs = self.registers;
+
+        regs.pwm_param[channel].write(PWM_PARAM::PHASE_DELAY.val(0x00) + PWM_PARAM::BLINK_EN_0::CLEAR + PWM_PARAM::HTBT_EN_0::CLEAR);
+        regs.duty_cycle[channel].write(DUTY_CYCLE::A_0.val(duty_cycle) + DUTY_CYCLE::B_0.val(duty_cycle));
+
+        match channel {
+            0 => regs.pwm_en.modify(PWM_EN::EN_0::SET),
+            1 => regs.pwm_en.modify(PWM_EN::EN_1::SET),
+            2 => regs.pwm_en.modify(PWM_EN::EN_2::SET),
+            3 => regs.pwm_en.modify(PWM_EN::EN_3::SET),
+            4 => regs.pwm_en.modify(PWM_EN::EN_4::SET),
+            5 => regs.pwm_en.modify(PWM_EN::EN_5::SET),
+            // Unreachable
+            _ => {},
+        }
+
+        Ok(())
+    }
+
+    fn pwm_chan_stop(&self, channel: usize) -> Result<(), ErrorCode> {
+        if channel >= PWM_MAX_CHANS {
+            // Channel index start from 0 
+            return Err(ErrorCode::INVAL);
+        }
+        let regs = self.registers;
+
+        match channel {
+            0 => regs.pwm_en.modify(PWM_EN::EN_0::CLEAR),
+            1 => regs.pwm_en.modify(PWM_EN::EN_1::CLEAR),
+            2 => regs.pwm_en.modify(PWM_EN::EN_2::CLEAR),
+            3 => regs.pwm_en.modify(PWM_EN::EN_3::CLEAR),
+            4 => regs.pwm_en.modify(PWM_EN::EN_4::CLEAR),
+            5 => regs.pwm_en.modify(PWM_EN::EN_5::CLEAR),
+            // Unreachable
+            _ => {},
+        }
+
+        Ok(())
+    }
 }
 
 impl hil::pwm::Pwm for PWMCtrl{
@@ -99,11 +204,16 @@ impl hil::pwm::Pwm for PWMCtrl{
         frequency_hz: usize,
         duty_cycle: usize,
     ) -> Result<(), ErrorCode> {
-        unimplemented!();
+        // Setup: Internal configuration to achieve desired results
+        self.pwm_setup(frequency_hz as u32, duty_cycle)?;
+        // Start: On specified channel with requested duty cycle
+        self.pwm_chan_start(*channel, duty_cycle as u32)?;
+        Ok(())
     }
 
-    fn stop(&self, pin: &Self::Pin) -> Result<(), ErrorCode> {
-        unimplemented!();
+    fn stop(&self, channel: &Self::Pin) -> Result<(), ErrorCode> {
+        self.pwm_chan_stop(*channel)?;
+        Ok(())
     }
 
     fn get_maximum_frequency_hz(&self) -> usize {
